1ae2928592db21f074cb96dcdf5158c4200c1565
David vanMaanen <david@vanmaanen.us>
2015-03-06 15:20:12 -0500

    Added time table

--- app/src/main/java/us/vanmaanen/yinztracker/TimeTableActivity.java
+++ app/src/main/java/us/vanmaanen/yinztracker/TimeTableActivity.java
@@ -1,20 +1,82 @@
 package us.vanmaanen.yinztracker;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+//import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import us.vanmaanen.yinztracker.PlaceDB.Place;
 import us.vanmaanen.yinztracker.R;
+import us.vanmaanen.yinztracker.ViewHelpers.TimeTableAdapter;
 
 public class TimeTableActivity extends ActionBarActivity {
-
+    ListView timetable;
+    TimeTableAdapter ttAdapter;
+    Place place;
+    boolean done=false;
+    ProgressDialog progressDialog;
+    boolean initDone=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_table);
+        Intent intent=getIntent();
+        long id=intent.getLongExtra("Place_ID",-1);
+        Log.d("App","Place ID: "+id);
+        timetable=(ListView) this.findViewById(R.id.timeTableList);
+        place=Place.findById(Place.class, id);
+        place.FindRouteStops();
+        progressDialog=ProgressDialog.show(this,"","Loading...");
+        place.updateTimes(this.getString(R.string.api_key), new Runnable() {
+            @Override
+            public void run() {
+                start_adapter();
+                progressDialog.dismiss();
+                initDone=true;
+            }
+        });
+
+
+        new Thread(new updateTask()).start();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        done=true;
+    }
+    public void start_adapter(){
+        ttAdapter=new TimeTableAdapter(this, R.layout.activity_time_table_row, place.getRouteStopList());
+        timetable.setAdapter(ttAdapter);
+    }
+
+    class updateTask implements Runnable{
+
+        @Override
+        public void run() {
+            String key=getString(R.string.api_key);
+            while(!done) {
+                if (initDone) {
+                    ttAdapter.updateTimes(key, new Runnable() {
+                        @Override
+                        public void run() {
+                            ttAdapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+                try{
+                    Thread.sleep(60000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/us/vanmaanen/yinztracker/ViewHelpers/TimeTableAdapter.java
+++ app/src/main/java/us/vanmaanen/yinztracker/ViewHelpers/TimeTableAdapter.java
@@ -1,7 +1,75 @@
 package us.vanmaanen.yinztracker.ViewHelpers;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+import us.vanmaanen.yinztracker.AddEditActivity;
+import us.vanmaanen.yinztracker.PlaceDB.RouteStop;
+import us.vanmaanen.yinztracker.R;
+
 /**
  * Created by david on 3/1/15.
  */
-public class TimeTableAdapter {
+
+public class TimeTableAdapter extends ArrayAdapter<RouteStop> {
+    Context context;
+    int ResID;
+
+    List<RouteStop> data;
+
+    public TimeTableAdapter(Context context, int resource, List<RouteStop> objects) {
+        super(context, resource, objects);
+        this.context=context;
+        this.ResID=resource;
+        this.data=objects;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        if (convertView==null) {
+            LayoutInflater inflater =LayoutInflater.from(context);
+            convertView = inflater.inflate(this.ResID, parent, false);
+        }
+        TextView RtTv=(TextView) convertView.findViewById(R.id.route);
+        TextView DirTv=(TextView) convertView.findViewById(R.id.direction);
+        TextView StpTv=(TextView) convertView.findViewById(R.id.stop);
+        TextView Time=(TextView) convertView.findViewById(R.id.TimetoBus);
+        RouteStop curItem=this.data.get(position);
+        RtTv.setText(curItem.getRtId()+" : "+curItem.getRouteLabel());
+        DirTv.setText(curItem.getDirection());
+        StpTv.setText(curItem.getStopLabel());
+        List<Integer> next=curItem.getMinToBus();
+        if (next==null){
+            Time.setText("NA");
+        }
+        else{
+            Time.setText(""+next.get(0));
+        }
+
+        return convertView;
+    }
+
+    public void updateTimes(String key,final Runnable onDone){
+        final int UpdateRunning=data.size();
+        for (RouteStop RS: data){
+            RS.updateTimes(key,new Runnable() {
+                int curDone=0;
+                @Override
+                public void run() {
+                   curDone+=1;
+                    if(this.curDone==UpdateRunning){
+                        onDone.run();
+                    }
+                }
+            });
+        }
+    }
+
+
 }
