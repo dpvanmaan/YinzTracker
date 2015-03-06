94ba717157c44230f573b5a012708a2ed805e5cb
David vanMaanen <david@vanmaanen.us>
2015-03-06 15:22:12 -0500

    Updated Main Activity

--- app/src/main/java/us/vanmaanen/yinztracker/MainActivity.java
+++ app/src/main/java/us/vanmaanen/yinztracker/MainActivity.java
@@ -1,29 +1,87 @@
 package us.vanmaanen.yinztracker;
 
 import android.content.Intent;
+import android.database.DataSetObserver;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+
+import us.vanmaanen.yinztracker.PlaceDB.Place;
+import us.vanmaanen.yinztracker.RestAPI.BusRoute;
+import us.vanmaanen.yinztracker.ViewHelpers.MainListAdapter;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    ListView MainList;
+    static final int ADD_EDIT_RESULT=1;
+    List<Place> PlaceList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        this.MainList=(ListView) this.findViewById(R.id.PlaceList);
 
+        updateList();
+        this.MainList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Place place=(Place) parent.getItemAtPosition(position);
+
+                Intent intent= new Intent(getBaseContext(),TimeTableActivity.class);
+                intent.putExtra("Place_ID",place.getId());
+                startActivity(intent);
+            }
+        });
+    }
+    private void updateList(){
+        PlaceList=new ArrayList<Place>();
+        Iterator<Place> PlIter= Place.findAll(Place.class);
+        int count=0;
+        Place next;
+        while(PlIter.hasNext()) {
+            next=PlIter.next();
+            if (next==null){
+                Log.d("App","WTF"+count);
+            }
+            next.FindRouteStops();
+            this.PlaceList.add(next);
+            count=count+1;
+        }
+        if (this.PlaceList!=null) {
+            this.MainList.setAdapter(new MainListAdapter(this, R.layout.activity_main_row, this.PlaceList));
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case ADD_EDIT_RESULT :{
+                if (resultCode==RESULT_OK) {
+                    updateList();
+                }
+            };
+        }
     }
 
     @Override
@@ -48,7 +106,8 @@ public class MainActivity extends ActionBarActivity {
     public void ClickAddPlace(View view){
         //Add Place on Button Click... Launch Add_edit_activity
         Intent intent = new Intent(this, AddEditActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, ADD_EDIT_RESULT);
+        Log.w("App","End of Intent.");
 
     }
     public void InsertPlace(String name,Integer id){
--- app/src/main/java/us/vanmaanen/yinztracker/ViewHelpers/MainListAdapter.java
+++ app/src/main/java/us/vanmaanen/yinztracker/ViewHelpers/MainListAdapter.java
@@ -1,7 +1,40 @@
 package us.vanmaanen.yinztracker.ViewHelpers;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import us.vanmaanen.yinztracker.PlaceDB.Place;
+import us.vanmaanen.yinztracker.R;
+
 /**
  * Created by david on 3/1/15.
  */
-public class MainListAdapter {
+public class MainListAdapter extends ArrayAdapter<Place> {
+    List<Place> data;
+    Context context;
+    int ResId;
+
+    public MainListAdapter(Context context, int resource, List<Place> objects) {
+        super(context, resource, objects);
+        this.data=objects;
+        this.context=context;
+        this.ResId=resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        if (convertView==null){
+            LayoutInflater layoutInflater=LayoutInflater.from(context);
+            convertView= layoutInflater.inflate(this.ResId, parent, false);
+        }
+        TextView Tv= (TextView) convertView.findViewById(R.id.RowText);
+        Tv.setText(this.data.get(position).getPlaceName());
+        return convertView;
+    }
 }
